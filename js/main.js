2053836c0873f7cc6a19e02e1d61c7b4f387308a
kashaf12 <kashafaahmed@gmail.com>
2020-07-14 19:11:29 +0530

    final changes done and project completed

--- js/main.js
+++ js/main.js
@@ -175,24 +175,7 @@
         "retina_detect": true
       });
     
-    //===== Progress Bar
-    
-    if($('.progress-line').length){
-        $('.progress-line').appear(function(){
-            var el = $(this);
-            var percent = el.data('width');
-            $(el).css('width',percent+'%');
-        },{accY: 0});
-    }
-    
-    
-    //===== Counter Up
-    
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1600,
-    });
-    
+
     
     //===== Magnific Popup
     
